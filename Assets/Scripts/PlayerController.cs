b8826dafb0b7ea2e004d6143a5533b29b66ca577
Jason Clarke <jclarke@student.neumont.edu>
2020-07-22 16:32:57 -0600

    New character on load

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 [SerializeField] float m_speed = 5.0f; 
+[SerializeField] Animator m_animator = null;
+
 void Update() { 
-	transform.Translate(Input.GetAxis("Vertical") * transform.forward * m_speed * Time.deltaTime); 
-	transform.Translate(Input.GetAxis("Horizontal") * transform.right * m_speed * Time.deltaTime); 
+	Vector3 forward = (Input.GetAxis("Vertical") * transform.forward * m_speed * Time.deltaTime);
+	transform.Translate(forward); 
+	Vector3 side = (Input.GetAxis("Horizontal") * transform.right * m_speed * Time.deltaTime);
+	transform.Translate(side);
+	m_animator.SetFloat("Speed", forward.magnitude);
 } 
 
 }
