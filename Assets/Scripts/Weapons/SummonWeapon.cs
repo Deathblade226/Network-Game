07f8a2776709f1d5d88ab38ac70b820a99042284
Jason Clarke <jclarke@student.neumont.edu>
2020-07-25 15:18:58 -0600

    Summon Weapon

---
+++ Assets/Scripts/Weapons/SummonWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonWeapon : Weapon {
+
+[SerializeField] GameObject summonMonster = null;
+[SerializeField] int minSummons = 1;
+[SerializeField] int maxSummons = 1;
+
+private List<GameObject> Summons = new List<GameObject>();
+private bool canSummon = true;
+
+void Awake() { Type = "Summon"; }
+
+public void Attack() { canSummon = (Summons.Count <= maxSummons); }
+
+void Update() {
+    GameObject go = null;
+    if (attack.Target != "") go = AIUtilities.GetNearestGameObject(gameObject, attack.Target, 0, attack.Nc.Fov, attack.Nc.SeeThroughWalls);
+    if (go != null && canSummon) { 
+    canSummon = false;
+    
+    int summon = Random.Range(minSummons, maxSummons-Summons.Count);
+
+    for (int i = 0; i < summon; i++) { 
+    
+    } 
+
+    }         
+}
+
+}
