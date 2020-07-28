e62159701b523680b63295281b9a39fd10d3d2c7
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2020-07-28 10:23:56 -0600

    DOORS!!!
    Added animation to doors and allows the player to open and close them.

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Canvas prompt = null;
+    [SerializeField] Animator animator = null;
+
+    private bool guiOpen = false;
+    
+    void Update()
+    {
+        if(guiOpen)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetBool("Open", !animator.GetBool("Open"));
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            prompt.enabled = true;
+            guiOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            prompt.enabled = false;
+            guiOpen = false;
+        }
+    }
+}
