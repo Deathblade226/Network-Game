f7ab4dcddd4e860375901e0bffd30e05e8e6e246
Jason Clarke <jclarke@student.neumont.edu>
2020-07-28 10:23:06 -0600

    Work

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,11 +12,13 @@ public class PlayerController : MonoBehaviour {
 [SerializeField] GameObject m_weapon = null;
 
 private void Update() {
+	if (m_weapon != null && !m_weapon.activeSelf) { 
 	if (Input.GetMouseButtonDown(0)) {  
 	m_animator.SetLayerWeight(1,1);
 	m_animator.SetTrigger("Attack"); 
 	} else if (!m_animator.GetCurrentAnimatorStateInfo(1).IsName("Male Attack 2")) { m_animator.SetLayerWeight(1, 0); }
 	else { m_animator.SetLayerWeight(1, 1); }
+	}
 }
 
 void FixedUpdate() { 
@@ -36,6 +38,11 @@ void FixedUpdate() {
 	m_animator.SetFloat("Speed Z", Input.GetAxis("Horizontal") * mod);
 
 	m_animator.SetBool("Moving", (velocity.z != 0 || torque.x != 0));
+
+	if (m_weapon != null) { 
+	if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Tab)) { m_weapon.SetActive((m_weapon.activeSelf) ? false : true); }
+	}
+
 } 
 
 }
--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -10,10 +10,23 @@ public override void Attack() { hit = false; }
 
 private void Awake() { Type = "Melee"; }
 private void Update() {
+    if (gameObject.tag == "Monster") { MonsterAttack(); }
+}
+
+private void MonsterAttack() { 
     GameObject go = null;
     if (attack.Target != "") go = AIUtilities.GetNearestGameObject(gameObject, attack.Target, 0, attack.Nc.Fov, attack.Nc.SeeThroughWalls);
     if (go != null && !hit) { 
     hit = true;
+    Damagable health = go.GetComponent<Damagable>();    
+    if (health != null) health.ApplyDamage(Damage);
+    if (DestroyOnHit) Destroy(gameObject);
+    }   
+}
+private void PlayerAttack() { 
+    GameObject go = null;
+    go = AIUtilities.GetNearestGameObject(gameObject, attack.Target, 0, attack.Nc.Fov, attack.Nc.SeeThroughWalls);
+    if (go != null) { 
     Damagable health = go.GetComponent<Damagable>();    
     if (health != null) health.ApplyDamage(Damage);
     if (DestroyOnHit) Destroy(gameObject);
