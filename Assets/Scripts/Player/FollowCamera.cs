948bf10bd9878ae945ff996fe1e7c890ab4913db
Jason Clarke <jclarke@student.neumont.edu>
2020-08-02 23:29:47 -0600

    Follow Cam

---
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+[SerializeField] Transform m_target = null;
+[SerializeField] float m_distance = 5.0f;
+[SerializeField] float m_smoothCam = 10.0f;
+
+float Pitch { get; set; } = 20;
+float Yaw { get; set; } = 0;
+
+void Start() {
+    Cursor.lockState = CursorLockMode.Locked;       
+}
+
+void Update() {
+
+}
+
+private void LateUpdate() {
+    if (m_target != null) { 
+    Quaternion rotation = m_target.rotation * Quaternion.AngleAxis(Yaw, Vector3.up) * Quaternion.AngleAxis(Pitch, Vector3.right);
+    Vector3 newPosition = m_target.position + rotation * new Vector3(0,0, -m_distance);
+
+    if (Physics.Raycast(m_target.position, newPosition - m_target.position, out RaycastHit hit)) {
+    transform.position = hit.point;
+    }
+    transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * m_smoothCam);
+
+    transform.LookAt(m_target);
+    }
+}
+
+}
